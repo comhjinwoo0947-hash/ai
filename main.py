d5ac9e18c1b471f34ffede996e1d3b8e4d26da28
comhjinwoo0947-hash <comhjinwoo0947@gmail.com>
2025-11-04 14:00:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
 st.write('안녕하세요, 만나서 반갑습니다!')
 name=st.text_input('이름을 입력해주세요!')
-st.selectbox('좋아하는 음식을 선택해주세요','치킨','준혁이','떡볶이','바나나'])
+st.selectbox('좋아하는 음식을 선택해주세요',['치킨','준혁이','떡볶이','바나나'])
 if st.button('인사말 생성'):
   st.write(name+'님! 안녕하세요!')
